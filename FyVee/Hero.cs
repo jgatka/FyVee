4323c4f2e9225c27859a7e6b7da5b56e0857618a
Josh Gatka <37256334+jgatka@users.noreply.github.com>
2019-01-06 21:37:14 -0500

    added the player name and character name selection logic

---
+++ FyVee/Hero.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FyVee
+{
+    class Hero
+    {
+
+        //Player name...the name of the person who is playing the role of the character
+        private string playerName;
+
+        public string PlayerName
+        {
+            get { return playerName; }
+            set { playerName = value; }
+        }
+
+        //characer name...the name of the character
+        private string characterName;
+
+        public string CharacterName
+        {
+            get { return characterName; }
+            set { characterName = value; }
+        }
+
+    }
+}
---
+++ FyVee/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//TODO - create a name generator for character names
+
+namespace FyVee
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string PressAnyKey = "Press any key to continue...";
+            #region Races
+            //list of available races
+            List<string> Races = new List<string>();
+            Races.Add("Dwarf");
+            Races.Add("Elf");
+            Races.Add("Halfling");
+            Races.Add("Human");
+            Races.Add("Dragonborn");
+            Races.Add("Gnome");
+            Races.Add("Half-Elf");
+            Races.Add("Half-Orc");
+            Races.Add("Tiefling");
+
+            #endregion
+
+            //Create a new hero
+            var CurrentHero = new Hero();
+
+            //Greet the user
+            Console.WriteLine("Welcome to FyVee, the 5th edition character creater!");
+            Console.WriteLine(PressAnyKey);
+            Console.ReadLine();
+            
+            //Get player name
+            Console.WriteLine("What is the name of the person who will be playing the role of this character?");
+            CurrentHero.PlayerName = Console.ReadLine();
+
+            //Get character name
+            Console.WriteLine("What is the name of the new character?");
+            CurrentHero.CharacterName = Console.ReadLine();
+
+            //TODO - Select a race
+
+            
+        }
+    }
+}
